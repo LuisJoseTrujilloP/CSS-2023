9ac150edacd3ced2a4d7043eed488de1e0e5cf47
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2023-07-31 01:45:51 -0400

    First function, working with regular functions

---
+++ src/00-let-const.js
@@ -0,0 +1,8 @@
+/// before we only used var such as>
+var LastName = "David";
+lastName = "Oscar";
+let fruit = 'Apple';
+fruit = 'Kiwi';
+return console.log(fruit, lastName);
+
+
---
+++ src/01-arrow-function.js
@@ -0,0 +1,12 @@
+function square(num){   
+    let result = num * num;
+    return result
+} 
+
+
+
+
+
+
+//print results
+console.log(square(3));
