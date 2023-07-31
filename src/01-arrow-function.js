0397e2cb2e71e1f48f5ce37ffe58759520487ca7
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2023-07-31 01:50:45 -0400

    Adding comments about arrowfunctions

--- src/01-arrow-function.js
+++ src/01-arrow-function.js
@@ -1,12 +1,14 @@
+// old way, we can change it to reduce the amount of code per function we need to create and a more freindly format
 function square(num){   
     let result = num * num;
     return result
 } 
 
-
-
+// NEW VERTION WITH ECMASCRIPT
+const squareNew = num => num * num;
 
 
 
 //print results
 console.log(square(3));
+console.log(squareNew(3));
