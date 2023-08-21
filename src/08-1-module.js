82b0bd59469f12dea83915070c45afa38fb83aa7
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2023-08-21 13:55:20 -0400

    using export async to call get an array of data

--- src/08-1-module.js
+++ src/08-1-module.js
@@ -1,3 +1,20 @@
 import hello from './08-module.js';
 
 hello();
+
+
+// export async function getData(){
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve([{
+//                 movie:"El despertar de los michis",
+//                 year: 2021,
+//                 protagonist: "Mr michi"
+//             }, {
+//                 movie:"101 Michis",
+//                 year: 2001,
+//                 protagonist: "Tommy MIchs"
+//             }])
+//         }, 1000);
+//     });
+// } 
--- src/08-module.js
+++ src/08-module.js
@@ -4,3 +4,9 @@ const hello = () => {
 
 
 export default hello
+// import { getData } from './08-1-module08.js'
+
+// export function solution(){
+//     return getData().then(movies => movies)
+// }
+
