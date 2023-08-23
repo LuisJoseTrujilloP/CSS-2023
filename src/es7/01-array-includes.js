8b3e97856b6dae4ae9af5c4f089d1aa0ca6a0e10
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2023-08-23 16:42:56 -0400

    uderstanding Object.entries and other updates with es8

--- src/es7/01-array-includes.js
+++ src/es7/01-array-includes.js
@@ -1,3 +1,7 @@
 // INCLUDES  this new operator returns a bowlean if an array contains the value that we passed to it  
 let numbers = [ 9, 5, 1, 13, 47,  ]
 console.log(numbers.includes(5))
+
+const list = ['Oscar', 'David', 'Monica', 'Daniel']
+console.log(list.includes('David'))
+console.log(list.includes('david'))
---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,4 @@
+    // OBJECT.ENTRIES takes an object and makes and array based on the object we passed
+const countries = { IT : 'Italy	', DK : 'Denmark' , JP : 'Japan	' , CO : 'Colombia'}
+console.log(Object.entries(countries))
+
