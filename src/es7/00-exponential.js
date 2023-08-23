533cb7395dd59bab260aec5e04f893c58597df89
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2023-08-23 16:29:55 -0400

    First steps on es7

---
+++ src/es7/00-exponential.js
@@ -0,0 +1,4 @@
+// a onperator was added, now we can use ** to provide the pow of a numeber
+
+const data = 3 ** 4
+console.log(data)
---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,3 @@
+// INCLUDES  this new operator returns a bowlean if an array contains the value that we passed to it  
+let numbers = [ 9, 5, 1, 13, 47,  ]
+console.log(numbers.includes(5))
