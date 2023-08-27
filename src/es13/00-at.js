fda9a6a22c9719779d29742f11954deda47c89b7
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2023-08-26 22:53:27 -0400

    Working with new methods of an array

---
+++ src/es13/00-at.js
@@ -0,0 +1,13 @@
+// AT to access information
+const array = ['One','Two','Three','Four','Five','Six']
+
+
+// MY WAY BEFORE SEE THE CODE
+const select = array[array.length - 2]
+
+
+// THIS IS A NEW WAY TO SELECT ARRAY.LENGHT WITH JUST A at(- # )
+const newWay = array.at(-1)
+
+console.log(select)
+console.log(newWay)
